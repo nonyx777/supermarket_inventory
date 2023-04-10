7d167b072a4e230cac1858d66f18144ff0384dce
nonyx777 <nahomretebo@gmail.com>
2023-04-11 00:12:25 +0300

    minor changes

---
+++ lib/View/Store/StoreManagement.dart
@@ -0,0 +1,287 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supermarket_inventory/Bloc/Store/bloc/store_bloc.dart';
+import 'package:supermarket_inventory/Data/Model/Product.dart';
+import 'package:supermarket_inventory/Service/ApiService.dart';
+import 'package:supermarket_inventory/Service/Utility.dart';
+import 'package:supermarket_inventory/View/Store/ProductListTile.dart';
+
+class StoreManagement extends StatefulWidget {
+  const StoreManagement({super.key});
+
+  @override
+  State<StoreManagement> createState() => _StoreManagementState();
+}
+
+class _StoreManagementState extends State<StoreManagement> {
+  int index = 0;
+
+  @override
+  void initState() {
+    BlocProvider.of<StoreBloc>(context).add(GetCategoryButtonPressed());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 20, 33, 61),
+        title: const Text(
+          style: TextStyle(
+            color: Color.fromARGB(255, 252, 163, 17),
+            fontSize: 25,
+          ),
+          "Products",
+        ),
+      ),
+      body: BlocBuilder<StoreBloc, StoreState>(
+        builder: (context, state) {
+          if (state is StoreInitialState) {
+            return Container(
+              child: Padding(
+                padding: EdgeInsets.all(height * 0.03),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          "Total Product Price",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Text(
+                          "Stock in Hand",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    //...
+                    SizedBox(
+                      height: height * 0.01,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Text(
+                          "0",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
+                        ),
+                        Text(
+                          "0",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                    //...
+                    SizedBox(
+                      height: height * 0.02,
+                    ),
+                    const Text(
+                      "Product List",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                      ),
+                    ),
+                    //...
+                    const Text(
+                      "No available products",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          } else if (state is StoreLoadingState) {
+            return Container(
+              child: Padding(
+                padding: EdgeInsets.all(height * 0.03),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          "Total Product Price",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Text(
+                          "Stock in Hand",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    //...
+                    SizedBox(
+                      height: height * 0.01,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Text(
+                          "0",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
+                        ),
+                        Text(
+                          "0",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                    //...
+                    SizedBox(
+                      height: height * 0.02,
+                    ),
+                    //product list(scrollable)
+                    const Text(
+                      "Product List",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                      ),
+                    ),
+                    //...
+                    SizedBox(
+                      height: height * 0.1,
+                    ),
+                    const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          } else if (state is StoreFailState) {
+            return Center(
+              child: Text(state.message),
+            );
+          } else if (state is StoreSuccessState) {
+            return Container(
+              child: Padding(
+                padding: EdgeInsets.all(height * 0.03),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            "Total Product Price",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            "Stock in Hand",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                      //...
+                      SizedBox(
+                        height: height * 0.01,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            "\$$totalProductPrice",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                          const Text(
+                            "2345",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                        ],
+                      ),
+                      //...
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      //product list(scrollable)
+                      const Text(
+                        "Product List",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                        ),
+                      ),
+                      //....
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      //....
+
+                      //display all the available categories from the api
+                      for (var product in state.product)
+                        Container(
+                          child: Column(
+                            children: [
+                              ProductListTile(
+                                productCategory: product.productCategory,
+                                productImage: product.productImage,
+                                productPrice:
+                                    productTotalPrice[product.productCategory],
+                              ),
+                              //....
+                              SizedBox(
+                                height: height * 0.015,
+                              ),
+                            ],
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }
+          return Container();
+        },
+      ),
+    );
+  }
+}
