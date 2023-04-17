c14dd58aede028e9f26c9704f9cd4eb40491cf2e
nonyx777 <nahomretebo@gmail.com>
2023-04-17 12:25:57 +0300

    total product price and stock in hand corrected and implemented respectively

--- lib/Service/Utility.dart
+++ lib/Service/Utility.dart
@@ -2,6 +2,7 @@ List<String> categoryExists = [];
 Map<String, dynamic> productTotalPrice = {};
 Map<String, dynamic> productTotalInCategory = {};
 double totalProductPrice = 0;
+double totalProductQuantity = 0;
 String selectedCategory = "";
 List<Map<String, dynamic>>? productData;
 List addedProducts = [];
