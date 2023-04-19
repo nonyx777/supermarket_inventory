7ecf74b97cb4f73f8b926c69887836795c0587b1
hewansirak <hewansg@gmail.com>
2023-04-19 16:08:58 +0300

    Constant colors

---
+++ assets/color/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const blueBlack = Color(0xFF14213D);
+const orangeAccent = Color(0xFFFCA311);
+const greyishWhite = Color(0xFFE5E5E5);
+const pureWhite = Colors.white;
